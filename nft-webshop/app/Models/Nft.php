f50e3e27b6881c637497c2a0d5aacf93ff9ce8f7
Vanja Rosic <vanjarosic01@gmail.com>
2024-01-16 01:26:02 +0100

    Kreirani modeli NFT i NFTCollection

---
+++ nft-webshop/app/Models/Nft.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Nft extends Model
+{
+    use HasFactory;
+
+    protected $table = 'nfts';
+
+    protected $fillable = [
+        'name',
+        'description', 
+        'price',
+        'crypto_currency',
+        'nft_collection_id',
+        'user_kupuje_id',
+    ];
+}
