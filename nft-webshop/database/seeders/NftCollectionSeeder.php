5b5223ffb7559535711e654b242640f73e87df13
Djordje Mirkovic <djordjemirkovic001@gmail.com>
2024-01-16 23:27:57 +0100

    Uskladjen NftCollectionSeeder kako bi se mogao testirati SearchController

--- nft-webshop/database/seeders/NftCollectionSeeder.php
+++ nft-webshop/database/seeders/NftCollectionSeeder.php
@@ -16,10 +16,18 @@ class NftCollectionSeeder extends Seeder
     {
 
 
-        for ($i = 1; $i <= 6; $i++) {
+        for ($i = 1; $i <= 3; $i++) {
             NftCollection::factory()->create([
                 'user_prodaje_id' => rand(1, 3),
                 'user_kupuje_id' => rand(4, 6),
+            ]);
+        }
+
+        for ($i = 1; $i <= 3; $i++) {
+            NftCollection::factory()->create([
+                'user_prodaje_id' => rand(1, 3),
+                'user_kupuje_id' => rand(4, 6),
+                'crypto_currency' => 'Ethereum'
             ]);
         }
     }
