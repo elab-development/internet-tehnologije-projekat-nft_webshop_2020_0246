3e8cf4389918e673ed7de7f8dfe690b3288cd27f
Vanja Rosic <vanjarosic01@gmail.com>
2024-01-28 15:37:25 +0100

    Dodatno sredjena Home komponenta i dodat njen css

--- nft-frontend/src/components/Home.jsx
+++ nft-frontend/src/components/Home.jsx
@@ -1,11 +1,29 @@
 
+import Footer from './Footer';
+import '../css/Home.css';
+
+
 const Home = () => {
 
     
     return (
-      <div className="home-page">
-        <p> na home stranici ste</p>
+        <>
+      <div className="home">
+      <h2>Dobrodosli na NFT WEBSHOP aplikaciju!</h2>
+      <div className='homeDiv'>
+      <p>
+        NASA APLIKACIJA VAM NUDI:
+        <ul>
+          <li>Pregled dostupnih NFT-jeva za kupovinu</li>
+          <li>Kratak opis o nasoj aplikaciji na stranici About</li>
+          <li>Pregled najcesce koriscenih kriptovaluta sa kojima mozete 
+            kupiti neki od NFT-jeva na nasoj stranici</li>
+        </ul>
+      </p>
+      </div>   
       </div>
+      <Footer />
+      </>
     );
   };
   
