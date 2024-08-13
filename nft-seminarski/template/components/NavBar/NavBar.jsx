86750f3fe20c7b94c2327ef9722a8583f209008b
Djordje Mirkovic <djordjemirkovic001@gmail.com>
2024-08-13 16:05:38 +0200

    zapoceto kreiranje fronta za seminarski

---
+++ nft-seminarski/template/components/NavBar/NavBar.jsx
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from 'react'
+
+import Style from './NavBar.module.css'
+import {Discover} from '.index'
+import {HelpCenter} from '.index'
+import {Notification} from '.index'
+import {Profile} from '.index'
+import {Sidebar} from '.index'
+import {Button} from '../componentindex'
+
+
+import Image from "next/image"
+import Link from "next/link"
+//IKONICE
+import {MdNotifications} from 'react-icons/md'
+import {BsSearch} from 'react-icons/bs'
+import {CgMenuLeft,CgMenuRight} from 'react-icons/cg'
+
+const NavBar = () => {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
---
+++ nft-seminarski/template/pages/_app.js
@@ -0,0 +1,8 @@
+import "@/styles/globals.css";
+
+const MyApp = ({ Component, pageProps }) =>(
+  <Component {...pageProps} />
+)
+
+
+export default MyApp;
