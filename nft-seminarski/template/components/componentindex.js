acc370f68700d0f78d433f1bc3da8103224a430a
Djordje Mirkovic <djordjemirkovic001@gmail.com>
2024-08-16 14:15:46 +0200

    index-filter+kategorije+nft card

--- nft-seminarski/template/components/componentindex.js
+++ nft-seminarski/template/components/componentindex.js
@@ -3,5 +3,9 @@ import Footer from "./Footer/Footer";
 import NavBar from "./NavBar/NavBar";
 import HeroSection from "./HeroSection/HeroSection";
 import BigNFTSilder from "./BigNFTSlider/BigNFTSlider";
+import Title from "./Title/Title";
+import Category from "./Category/Category";
+import Filter from "./Filter/Filter";
+import NFTCard from "./NFTCard/NFTCard";
 
-export {Button, Footer, NavBar, HeroSection, BigNFTSilder};
+export {Button, Footer, NavBar, HeroSection, BigNFTSilder, Title, Category, Filter, NFTCard};
