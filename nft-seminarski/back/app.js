3ccb3b42b39edb54c9e84016fe7052d3394cc083
valja35 <vanjarosic01@gmail.com>
2024-09-10 12:55:09 +0200

    back - error handling

--- nft-seminarski/back/app.js
+++ nft-seminarski/back/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const AppError = require("./Utils/appError");
+const globalErrorHandler = require("./controllers/errorControllers");
 const nftsRouter = require("./routes/nftsRoute");
 const usersRouter = require("./routes/usersRoute");
 
@@ -14,7 +16,7 @@ app.use(express.static(`${__dirname}/nft-data/img`));
 
 //CUSTOM MIDDLE WARE
 app.use((req, res, next) => {
-  console.log("Hey i am from middleware function 👋");
+  console.log("Midlver omotac 👋");
   next();
 });
 
@@ -25,5 +27,21 @@ app.use((req, res, next) => {
 
 app.use("/api/v1/nfts", nftsRouter);
 app.use("/api/v1/users", usersRouter);
+
+app.all("*", (req, res, next) => {
+  /*res.status(404).json({
+    status:"fail",
+    message: `Nije moguce naci ${req.originalUrl} on this server`,
+
+  });
+  const err = new Error(`Nije moguce naci ${req.originalUrl} na serveru`);
+  err.status = "fail";
+  err.statusCode = 404;
+  next(err);*/
+  next(new AppError(`Nije moguce naci ${req.originalUrl} na serveru`,404));
+});
+//global
+app.use(globalErrorHandler);
+
 
 module.exports = app;
