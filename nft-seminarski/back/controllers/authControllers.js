18160185a8cef1a608a6ce74b307b0a084d7d928
valja35 <vanjarosic01@gmail.com>
2024-09-11 15:27:20 +0200

    back - cookie

--- nft-seminarski/back/controllers/authControllers.js
+++ nft-seminarski/back/controllers/authControllers.js
@@ -15,6 +15,32 @@ const signToken = id => {
 }
 
 
+
+const createSendToken = (user,statusCode,res)=>{
+    const token = signToken(user._id);
+
+
+
+    const cookieOptions = {
+        expires: new Date(
+        Date.now()+process.env.JWT_COOKIE_EXPIRE_IN*24*60*60*1000),
+        //secure: true,
+        httpOnly:true,
+        
+    }
+
+    res.cookie("jwt",token,cookieOptions)
+    user.password=undefined;
+
+    res.status(statusCode).json({
+        status: "success",
+        token,
+        data:{
+            user,
+        }
+    })
+}
+
 //SIGNUP
 exports.signup = catchAsync(async(req,res,next)=>{
     const newUser = await User.create(req.body);
